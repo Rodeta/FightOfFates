2fc4c11421e69fd1a32e30c4f0f854339d0fb6ca
janscho98 <Jan_Aaron.Schoenfeld@Student.Reutlingen-University.de>
2020-05-08 18:05:06 +0200

    Shooting scripts

---
+++ FightOfFates/Assets/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform FirePoint; //Should be type Transform
+    public GameObject Bullet;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+        
+    }
+
+    void Shoot()
+    {
+        Instantiate(Bullet, FirePoint.position, FirePoint.rotation);
+    }
+}
