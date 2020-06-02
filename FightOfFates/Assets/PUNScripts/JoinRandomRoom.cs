1df7d13c5a32545dbe4517b78897f5d13ec01a5d
AlexMarkPollok <alex_mark.pollok@student.reutlingen-university.de>
2020-06-02 21:36:58 +0200

    Joins Room when starting game

---
+++ FightOfFates/Assets/PUNScripts/JoinRandomRoom.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class JoinRandomRoom : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        UnityEngine.Debug.Log("We are now connected to the " + PhotonNetwork.CloudRegion + " server!");
+        PhotonNetwork.JoinRandomRoom(); //First tries to join existing room
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to join Random Room");
+        CreateRoom();
+    }
+
+    void CreateRoom()
+    {
+        Debug.Log("Creating new Room");
+        int randomRoomNumber = Random.Range(0, 10000); //creating a random name for the room
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 2 };
+        PhotonNetwork.CreateRoom("Room" + randomRoomNumber, roomOps); //attempting to create a new Room
+        Debug.Log(randomRoomNumber);
+
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to create a room, trying again...");
+        CreateRoom();
+    }
+
+    public override void OnPlayerEnteredRoom(Player other)
+    {
+        Debug.Log("OnPlayerEnteredRoom() "); // not seen if you're the player connecting
+    }
+
+
+    public override void OnPlayerLeftRoom(Player other)
+    {
+        Debug.Log("OnPlayerLeftRoom()"); // seen when other disconnects
+    }
+}
--- FightOfFates/Assets/Script/MainMenu.cs
+++ FightOfFates/Assets/Script/MainMenu.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Photon.Pun;
 
 public class MainMenu : MonoBehaviour
 {
     public void PlayTutorial()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        PhotonNetwork.LoadLevel(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
 
