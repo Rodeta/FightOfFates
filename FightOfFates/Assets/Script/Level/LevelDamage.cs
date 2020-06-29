0deb84582c4b169c2b99d7ab01cc3b4a628b01d3
Philip Dengler <Philip.Dengler@novatec-gmbh.de>
2020-06-29 10:18:34 +0200

    Lava and spikes adapted for basic player.

---
+++ FightOfFates/Assets/Script/Level/LevelDamage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public class LevelDamage : MonoBehaviour
+{
+
+    protected Player player;
+    public SelectPlayerController selectPlayerController;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player == null)
+        {
+            try
+            {
+                if (selectPlayerController.modus == 0)
+                {
+                    player = GameObject.Find("BasicPlayer(Clone)").GetComponent<BasicPlayerController>();
+                }
+                else
+                {
+                    player = GameObject.Find("Player(Clone)").GetComponent<PlayerController>();
+                }
+
+            }
+            catch (Exception e)
+            {
+                return;
+            }
+        }
+    }
+
+
+
+    protected void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
