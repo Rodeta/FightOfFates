2e55629a21d057171d87a5c3f3bee6faea27331e
Philip Dengler <Philip.Dengler@novatec-gmbh.de>
2020-07-13 10:10:22 +0200

    stuck fix and spikes updated

--- FightOfFates/Assets/Script/Level/Spike.cs
+++ FightOfFates/Assets/Script/Level/Spike.cs
@@ -1,13 +1,72 @@
 ﻿using System;
 using UnityEngine;
 
-public class Spike : LevelDamage
+public class Spike : MonoBehaviour
 {
 
+
+    public GameObject spike;
+    private BoxCollider2D spikeCollider;
+    private GameObject player;
+    private CapsuleCollider2D playerCollider;
+    private Player playerController;
+
+    private float lastAttack;
+
+
+    /*
     private int damage = 20;
 
     new void OnTriggerEnter2D(Collider2D collision)
     {
         player.TakeDamage(damage);
     }
+    */
+    private void Start()
+    {
+        spikeCollider = spike.GetComponent<BoxCollider2D>();
+        
+    }
+
+    private void Update()
+    {
+        if(player == null)
+        {
+            try
+            {
+                player = GameObject.Find("Player");
+                playerCollider = player.GetComponent<CapsuleCollider2D>();
+                playerController = player.GetComponent<Player>();
+            }
+            catch(Exception e)
+            {
+                return;
+            }
+        }
+
+        TackeDamage();
+    }
+
+
+
+    private void TackeDamage()
+    {
+
+        if (Time.time >= lastAttack + 0.2)
+        {
+
+            if (spikeCollider.IsTouching(playerCollider))
+            {
+                float[] attackDetails = new float[2];
+                attackDetails[0] = 2f;
+                attackDetails[1] = spike.transform.position.x;
+
+                playerController.TakeDamage(20);
+                lastAttack = Time.time;
+            }
+
+        }
+
+    }
+
 }
