42279ae3ad4abc6081ff37aa9a60c9fd33634a1b
Rodion Kraft <Rodion.Kraft@googlemail.com>
2020-07-17 20:04:07 +0200

    change spwanscene

--- FightOfFates/Assets/Script/Lobby/CounterScript.cs
+++ FightOfFates/Assets/Script/Lobby/CounterScript.cs
@@ -115,7 +115,7 @@ public class CounterScript : MonoBehaviour, IOnEventCallback
         }
         if (PhotonReady && LocalReady)
         {
-            PhotonNetwork.LoadLevel("FinalStage");
+            PhotonNetwork.LoadLevel("JumpAndRun");
             PhotonNetwork.AutomaticallySyncScene = false;
         }
     }
@@ -130,7 +130,7 @@ public class CounterScript : MonoBehaviour, IOnEventCallback
 
             string test = (string)data[0];
 
-            PhotonNetwork.LoadLevel("FinalStage");
+            PhotonNetwork.LoadLevel("JumpAndRun");
             PhotonNetwork.AutomaticallySyncScene = false;
 
         }
